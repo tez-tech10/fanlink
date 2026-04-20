4022837cf7cd37f5570da9db5860ee390df618d8
tez-tech10 <meemooto@gmail.com>
2026-04-21 02:40:56 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
+const { Pool } = require('pg');
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
-);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
-module.exports = supabase;
+module.exports = pool;
